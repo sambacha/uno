560fdc6df5cfd5c363e1df9126f504b16cb625ea
Sam Bacha <sam@freighttrust.com>
2020-09-28 07:36:55 +0000

    inital github commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: ISC
+"use strict";
+
+import wrappy from 'wrappy';
+export default wrappy(uno);
+export const strict = wrappy(unoStrict);
+uno.proto = uno(function () {
+    Object.defineProperty(Function.prototype, 'uno', {
+        value: function () {
+            return uno(this);
+        },
+        configurable: true
+    });
+    Object.defineProperty(Function.prototype, 'unoStrict', {
+        value: function () {
+            return unoStrict(this);
+        },
+        configurable: true
+    });
+});
+function uno(fn) {
+    class f {
+		constructor() {
+			if (f.called)
+				return f.value;
+			f.called = true;
+			return f.value = fn.apply(this, arguments);
+		}
+	}
+    f.called = false;
+    return f;
+}
+function unoStrict(fn) {
+    class f {
+		constructor() {
+			if (f.called)
+				throw new Error(f.unoError);
+			f.called = true;
+			return f.value = fn.apply(this, arguments);
+		}
+	}
+    var name = fn.name || 'Function wrapped with `uno`';
+    f.unoError = name + " shouldn't be called more than uno";
+    f.called = false;
+    return f;
+}
+
